cb2986333a1344170874d306c90a688442a54269
MatiasMiguelGomez <miguel_gomez30@outlook.com>
2024-11-29 15:25:49 -0300

    add: SPA page in progress

--- webapp-seleccionado/src/App.jsx
+++ webapp-seleccionado/src/App.jsx
@@ -1,8 +1,22 @@
 import CreationsElements from './modules/pages/CreationsElements/CreationsElements.jsx';
+import Navbar from './modules/components/navBar/navBar.jsx';
+import Footer from './modules/components/footer/footer.jsx';
+import Home from './modules/pages/Home-page/Home.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-      <CreationsElements></CreationsElements>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route
+            path='/crear-juego'
+            element={<CreationsElements></CreationsElements>}
+          ></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </>
   );
 }
