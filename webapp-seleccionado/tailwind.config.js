e44c6ec9a6109fc87e017fccb37ac2c8bb343444
Morfeo <gastongomez2014@hotmail.com>
2024-11-30 16:38:07 -0300

    Se agregaron las waves y unos pequeños cambios en los estilos del tailwind.config para que sea mas fiel al diseño

--- webapp-seleccionado/tailwind.config.js
+++ webapp-seleccionado/tailwind.config.js
@@ -3,11 +3,18 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+    	fontFamily: {
+        	pangolin: ["Pangolin", "cursive"],
+      	},
       colors: {
         blueSecondary: '#2546A5',
         blueButton: '#6F8DE5',
         whitePrimary: '#E4EBFF',
+        primaryCyan: "#6F8DE5",
+        secondaryBlue: '#2546A5',
+        purpleAccent: '#D532D9'
       },
+      
     },
   },
   plugins: [],
