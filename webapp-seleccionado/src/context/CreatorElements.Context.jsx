750a24f53e3ad6967b650e33a15487dbec24d9c5
MatiasMiguelGomez <miguel_gomez30@outlook.com>
2024-12-10 18:30:56 -0300

    fixed the logo

--- webapp-seleccionado/src/context/CreatorElements.Context.jsx
+++ webapp-seleccionado/src/context/CreatorElements.Context.jsx
@@ -20,75 +20,12 @@ const CreatorElementProvider = ({ children }) => {
 
   const [visibleForm, setVisibleForm] = useState(null);
 
-  // const savedGames = newGame => {
-  //   const games = JSON.parse(localStorage.getItem('games')) || [];
-  //   const addingLevelArray = [...games, newGame];
-  //   const updatedGames = addingLevelArray.map((g, index) => {
-  //     return { ...g, id: index + 1 };
-  //   });
-  //   const findedGame = updatedGames.find(g => g.title === newGame.title);
-  //   console.log(findedGame);
-  //   Swal.fire({
-  //     title: '¡Juego finalizado!',
-  //     text: 'El juego se ha creado con éxito. ¿Qué deseas hacer ahora?',
-  //     icon: 'success',
-  //     showCancelButton: true,
-  //     confirmButtonText: 'Ir a jugarlo',
-  //     cancelButtonText: 'Volver al inicio',
-  //   }).then(result => {
-  //     if (result.isConfirmed) {
-  //       window.location.href = `/play-game/${findedGame.id}`;
-  //     } else {
-  //       window.location.href = '/';
-  //     }
-  //   });
-  //   localStorage.setItem('games', JSON.stringify(updatedGames));
-  // };
-
-  // const savedGames = newGame => {
-  //   console.log('savedGames function called');
-  //   const games = JSON.parse(localStorage.getItem('games')) || [];
-
-  //   // Obtiene el último ID y suma 1 para el nuevo juego
-  //   const lastId = games.length > 0 ? games[games.length - 1].id : 0;
-  //   const newId = lastId + 1;
-
-  //   // Crea el nuevo juego con el ID asignado
-  //   const newGameWithId = { ...newGame, id: newId };
-
-  //   const updatedGames = [...games, newGameWithId];
-
-  //   // Buscar el juego recién creado con su ID
-  //   const findedGame = updatedGames.find(g => g.id === newId);
-
-  //   console.log(findedGame);
-
-  //   // Mostrar la alerta con SweetAlert
-  //   Swal.fire({
-  //     title: '¡Juego finalizado!',
-  //     text: 'El juego se ha creado con éxito.',
-  //     icon: 'success',
-  //     showCancelButton: true, // Mantén el botón de cancelación
-  //     cancelButtonText: 'Volver al inicio', // Solo el botón "Volver al inicio"
-  //   }).then(result => {
-  //     if (result.isDismissed) {
-  //       // Verifica si el botón de cancelar fue presionado
-  //       window.location.href = '/'; // Redirige al inicio
-  //     }
-  //   });
-
-  //   // Guardar los juegos actualizados en el localStorage
-  //   localStorage.setItem('games', JSON.stringify(updatedGames));
-  // };
-
   const savedGames = newGame => {
     const games = JSON.parse(localStorage.getItem('games')) || [];
     const addingLevelArray = [...games, newGame];
     const updatedGames = addingLevelArray.map((g, index) => {
       return { ...g, id: index + 1 };
     });
-    const findedGame = updatedGames.find(g => g.title === newGame.title);
-    console.log(findedGame);
     Swal.fire({
       title: '¡Juego finalizado!',
       text: 'El juego se ha creado con éxito.',
